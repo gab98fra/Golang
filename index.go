04e347fc7f4905a811c380951690713532f35708
Gabriel F <gabriel_frac@outlook.com>
2022-09-19 02:26:06 -0500

    add prodcut

--- index.go
+++ index.go
@@ -1,8 +1,13 @@
 package main
-
 import "fmt"
 
 func main() {
-    fmt.Println("Hola mundo")
+    var num1 int
+    var num2 float32
+    var product float32
+    num1=232
+    num2=23.2
+    product=float32(num1) * num2
+    fmt.Println("result: ", product)
 }
 
