087199344b81f60cee634a7bdb140c57401425e5
Gabriel F <gabriel_frac@outlook.com>
2022-11-21 00:00:39 -0600

    repetion structures

---
+++ repetition_structures.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+
+func main(){
+	x :=1
+	for x <5{
+		fmt.Println("Hello World")
+		x +=1
+	}
+	
+	for index :=1;index<10;index++{
+
+		fmt.Println("Hi, how are you?")
+	}
+
+}
