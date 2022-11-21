1300a2427ff79de02f6d19dfda3c6e676b25aa00
Gabriel F <gabriel_frac@outlook.com>
2022-11-20 22:20:22 -0600

    add function

--- index.go
+++ index.go
@@ -2,12 +2,30 @@ package main
 import "fmt"
 
 func main() {
+    //Variables
     var num1 int
     var num2 float32
-    var product float32
+    var param float32
     num1=232
     num2=23.2
-    product=float32(num1) * num2
-    fmt.Println("result: ", product)
+    /*
+        Param
+    */
+    param=float32(num1) * num2
+    fmt.Println("param: ", param)
+
+    //Execute function: get_salary
+    get_salary(param)
+}
+
+
+func get_salary(param float32){
+    var hours_worked int
+    var salary float32
+    fmt.Println("Give me hours worked")
+    fmt.Scan(&hours_worked)
+    salary=float32(hours_worked)*param
+    fmt.Println("Salary: ", salary)
+
 }
 
